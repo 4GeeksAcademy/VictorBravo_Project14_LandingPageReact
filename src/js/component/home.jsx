a57ead617458978852796aa5d3488bab8bf8e69c
Víctor Bravo <vbravo.quintana@gmail.com>
2024-08-23 19:02:48 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,55 @@
 import React from "react";
+import Navbar from "./navbar"
+import Jumbotron from "./jumbotron";
+import Card from "./card";
+import Footer from "./footer";
+import placeholderImage from "../../img/placeholder.png"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+//create your Home component
 const Home = () => {
+	const cards = [
+		{
+			imgSrc: placeholderImage,
+			title: 'Card title 1',
+			text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+		},
+		{
+			imgSrc: placeholderImage,
+			title: 'Card title 2',
+			text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+		},
+		{
+			imgSrc: placeholderImage,
+			title: 'Card title 3',
+			text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+		},
+		{
+			imgSrc: placeholderImage,
+			title: 'Card title 4',
+			text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+		},
+		{
+			imgSrc: placeholderImage,
+			title: 'Card title 5',
+			text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+		}, {
+			imgSrc: placeholderImage,
+			title: 'Card title 6',
+			text: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+		},
+	];
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<Navbar />
+			<Jumbotron />
+			<div className="d-flex justify-content-center row row-cols-1 row-cols-md-3 g-4 p-5">
+				{cards.map((card, index) => (
+					<Card title={card.title} text={card.text} imgSrc={card.imgSrc} />
+				))}
+			</div>
+			<Footer />
+		</>
 	);
 };
 
